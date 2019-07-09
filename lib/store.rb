0b2a580a0f67b89bd77f775de7eed6067a649cf6
Aidanchase <aidanmro@gmail.com>
2019-07-09 20:03:51 +0000

    done

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,6 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates_length_of :name, :minimum => 3
+  validates :annual_revenue, length: { in: 1...100000000000000000000 }
+  validates_associated :employees
 end
